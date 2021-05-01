5012bcc2c4e04f98db5df6eaffcfa83a8e72eff8
Sam <samuelholt123@gmail.com>
2021-05-01 09:03:24 -0400

    inital commit

---
+++ PingVolumeControl.plugin.js
@@ -0,0 +1,9 @@
+/**
+ * @name PingVolumeControl
+ * @author Sam Holt
+ * @description A plugin designed to allow users to control the volume of incoming pings
+ * @version 0.0.1
+ * @authorLink https://github.com/Cthulhuhub
+ * @website https://green-eggs-and-sam.com/
+ * @source
+ */
